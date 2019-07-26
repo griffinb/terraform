51ddbf5fb1267a656799d873bf9b8fa4f1e2d381
Brandon Griffin <brandon.griffin@rackspace.com>
2019-07-26 08:41:16 -0500

    Cleanup

--- main.tf
+++ main.tf
@@ -2,27 +2,45 @@ provider "aws" {
     region = "us-east-1"
 }
 
-data "aws_availability_zones" "all" {}
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
 
 variable "server_port" {
     description = "The port the server will use for HTTP requests"
-    default = 8080
+    default     = 8080
+}
+
+output "elb_dns_name" {
+    value = "${aws_elb.example.dns_name}"
 }
 
 resource "aws_security_group" "elb" {
     name = "terraform-example-elb"
 
     ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
 
     egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
         cidr_blocks = ["0.0.0.0/0"]
     }
 }
@@ -44,36 +62,35 @@ resource "aws_security_group" "instance" {
 
 resource "aws_elb" "example" {
     name = "terraform-asg-example"
-    availability_zones = ["${data.aws_availability_zones.all.names}"]
+    availability_zones = ["us-east-1a"]
     security_groups = ["${aws_security_group.elb.id}"]
 
     listener {
-        lb_port = 80
-        lb_protocol = "http"
-        instance_port = "${var.server_port}"
+        lb_port           = 80
+        lb_protocol       = "http"
+        instance_port     = "${var.server_port}"
         instance_protocol = "http"
     }
 
     health_check {
-        healthy_threshold = 2
+        healthy_threshold   = 2
         unhealthy_threshold = 2
-        timeout = 3
-        interval = 30
-        target = "HTTP:${var.server_port}/"
+        timeout             = 3
+        interval            = 30
+        target              = "HTTP:${var.server_port}/"
     }
-
 }
 
 resource "aws_launch_configuration" "example" {
-    image_id = "ami-40d28157"
+    image_id      = "${data.aws_ami.ubuntu.id}"
     instance_type = "t2.micro"
     security_groups = ["${aws_security_group.instance.id}"]
 
     user_data = <<-EOF
-        #!/bin/bash
-        echo "Hello, World" > index.html
-        nohup busybox httpd -f -p "${var.server_port}" &
-        EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p "${var.server_port}" &
+                EOF
 
     lifecycle {
         create_before_destroy = true
@@ -82,21 +99,17 @@ resource "aws_launch_configuration" "example" {
 
 resource "aws_autoscaling_group" "example" {
     launch_configuration = "${aws_launch_configuration.example.id}"
-    availability_zones = ["${data.aws_availability_zones.all.names}"]
+    availability_zones   = ["us-east-1a"]
 
-    load_balancers = ["${aws_elb.example.name}"]
+    load_balancers    = ["${aws_elb.example.name}"]
     health_check_type = "ELB"
 
     min_size = 2
     max_size = 10
 
     tag {
-        key = "Name"
-        value = "terraform-asg-example"
+        key                 = "Name"
+        value               = "terraform-asg-example"
         propagate_at_launch = true
     }
-}
-
-output "elb_dns_name" {
-    value = "${aws_elb.example.dns_name}"
 }
